1b9a10002c2b2074652ca31e3673a9197546950f
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-06-03 19:13:25 -0400

    feat: tela de carrinho

---
+++ app/(tabs)/Carrinho.tsx
@@ -0,0 +1,114 @@
+import { useFocusEffect } from "@react-navigation/native";
+import { router } from "expo-router";
+import { useCallback, useState } from "react";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { obterCarrinho, removerDoCarrinho } from "../../utils/carrinho";
+
+export default function Carrinho() {
+  const [produtos, setProdutos] = useState(() => obterCarrinho());
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log("Tela em foco");
+      setProdutos(obterCarrinho());
+      return () => {
+        console.log("Tela perdeu o foco");
+      };
+    }, [])
+  );
+
+  const handleRemover = (id: string) => {
+    removerDoCarrinho(id);
+    setProdutos([...obterCarrinho()]);
+    Alert.alert("Removido", "Produto removido do carrinho.");
+  };
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Carrinho de Compras</Text>
+      {produtos.map((produto) => (
+        <View key={produto.id} style={styles.item}>
+          <TouchableOpacity
+            onPress={() =>
+              router.push({
+                pathname: "/DetalhesProduto",
+                params: {
+                  id: produto.id.toString(),
+                  nome: produto.nome,
+                  preco: produto.preco.toString(),
+                  imagem: produto.imagem,
+                  quantidade: produto.quantidade,
+                  dosagem: produto.dosagem,
+                  marca: produto.marca,
+                },
+              })
+            }
+          >
+            <Image source={{ uri: produto.imagem }} style={styles.image} />
+          </TouchableOpacity>
+
+          <View style={styles.info}>
+            <Text style={styles.name}>{produto.nome}</Text>
+            <Text style={styles.details}>R$ {produto.preco.toFixed(2)}</Text>
+            <Text style={styles.details}>
+              {produto.quantidade} - {produto.dosagem}
+            </Text>
+            <TouchableOpacity onPress={() => handleRemover(produto.id)}>
+              <Text style={styles.remove}>Remover</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    marginTop: 32,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  item: {
+    flexDirection: "row",
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+    paddingBottom: 8,
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 8,
+    marginRight: 12,
+  },
+  info: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  details: {
+    fontSize: 14,
+    color: "#555",
+  },
+  remove: {
+    marginTop: 8,
+    fontSize: 14,
+    color: "red",
+  },
+});
