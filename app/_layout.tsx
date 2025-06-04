b959eed63ef3bf3ed76d9a851198f3932e2d958c
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-06-04 00:02:50 -0400

    refactor: removendo provedor de tema

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,10 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <>
       <Stack>
         {/* Tela inicial (login) - agora como index */}
         <Stack.Screen name="index" options={{ headerShown: false }} />
@@ -31,13 +13,13 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
         <Stack.Screen
-          name="Detalhes Produto"
+          name="DetalhesProduto"
           options={{ title: "Detalhes", headerShown: true }}
         />
         {/* Rota 404 */}
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
