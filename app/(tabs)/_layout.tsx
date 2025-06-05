3defa9bb051d871e31257f0e13c259fa7555bcfc
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-06-05 15:20:54 -0400

    feat: Oculta DetalhesProduto.tsx do TabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,45 @@
 import { Feather } from "@expo/vector-icons";
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
 export default function TabLayout() {
-
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color,size }) =>  <Feather name="home" size={size} color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="BuscaPorMedicamentos"
         options={{
-          title: 'Busca',
-          tabBarIcon: ({ color,size }) =>  <Feather name="search" size={size} color={color} />,
+          title: "Buscar",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="search" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="Carrinho"
         options={{
-          title: 'Carrinho',
-          tabBarIcon: ({ color,size }) =>  <Feather name="shopping-cart" size={size} color={color} />,
+          title: "Carrinho",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="shopping-cart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="DetalhesProduto"
+        options={{
+          href: null, // impede que apareça na tab bar
         }}
       />
     </Tabs>
